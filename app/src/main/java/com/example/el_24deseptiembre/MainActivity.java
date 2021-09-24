0060ef9e1d9d72c6d4010b6490323b1b823b0c2b
fimovol <fimovolpe@gmail.com>
2021-09-24 14:17:08 -0500

    agregando la interfaz al archivo de java principal

--- app/src/main/java/com/example/el_24deseptiembre/MainActivity.java
+++ app/src/main/java/com/example/el_24deseptiembre/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.el_24deseptiembre;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    DataBaseHelper mydb;
+    EditText etNombre,etApellido,etCorreo;
+    Button btRegistrar, btVerdatos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mydb = new DataBaseHelper(this);
+        etNombre = findViewById(R.id.editTextTextPersonName);
+        etApellido= findViewById(R.id.editTextTextPersonName2);
+        etCorreo = findViewById(R.id.editTextTextPersonName3);
+
+        btRegistrar = findViewById(R.id.button);
+        btVerdatos = findViewById(R.id.button2);
+
+
     }
 }
